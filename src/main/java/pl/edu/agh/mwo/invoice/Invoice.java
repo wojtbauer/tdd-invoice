8febe12a51d57bb812e3bba9c992ef639a96489c
Wojciech Frącz <fraczwojciech@gmail.com>
2018-02-25 21:56:41 +0100

    0 - Invoice number

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Map<Product, Integer> products = new HashMap<Product, Integer>();
+	private Map<Product, Integer> products = new HashMap<>();
+	private static int nextNumber = 0;
+	private final int number = ++nextNumber;
 
 	public void addProduct(Product product) {
 		addProduct(product, 1);
@@ -40,5 +42,9 @@ public class Invoice {
 			totalGross = totalGross.add(product.getPriceWithTax().multiply(quantity));
 		}
 		return totalGross;
+	}
+
+	public int getNumber() {
+		return number;
 	}
 }
