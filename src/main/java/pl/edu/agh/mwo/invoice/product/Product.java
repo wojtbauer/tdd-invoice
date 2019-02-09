32bd90d5f2044a7410aa3c72d05f735643f97fef
Wojciech Frącz <fraczwojciech@gmail.com>
2019-02-09 14:15:55 +0100

    Lab 2 progress

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,6 +10,9 @@ public abstract class Product {
 	private final BigDecimal taxPercent;
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
+		if (name == null) {
+			throw new IllegalArgumentException("Name cannot be null");
+		}
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
